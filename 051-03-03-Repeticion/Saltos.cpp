ef5b428b6ca32a9a59fb0dfacfdc772092b7a0ec
Pranieri <sebastian.ranieri@hotmail.com>
2017-06-14 00:07:07 -0300

    Create Saltos.cpp

---
+++ 051-03-03-Repeticion/Saltos.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+/*
+Nombre: Saltos
+Descripcion: Se repite una fase 30 veces
+Autor: Pablo Sebastian Ranieri Castillo
+Ultima fecha de modificacion: 13/6/2017
+*/
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    if (i<30)
+    {
+      cout<<i<<"Repeticion"<<endl;
+      i++;
+    }
+    
+    return main();
+}
