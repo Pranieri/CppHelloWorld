3836b46d1a5a0fca08ab391eb98d159e6e1c076e
Pranieri <sebastian.ranieri@hotmail.com>
2017-06-13 23:23:54 -0300

    Add files via upload

---
+++ 051-03-01-Adicion/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+/*
+Nombre: Adicion
+Se muestra por pantalla la suma de dos numeros
+Autor: Pablo Sebastian Ranieri Castillo
+Ultima fecha de modificacion: 13/6/2017
+
+*/
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Ingrese un numero" << endl;
+    cin>>a;
+    cout << "Ingrese otro" << endl;
+    cin>>b;
+
+    cout << "La suma es " <<a+b<<endl;
+
+    return 0;
+}
