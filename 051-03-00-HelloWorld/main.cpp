b8b6ffce31740316793042b6650e8ab959b7d5d2
Pranieri <sebastian.ranieri@hotmail.com>
2017-06-13 22:51:51 -0300

    Add files via upload

---
+++ 051-03-00-HelloWorld/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+/* Nombre: Hello World
+Se muestra por pantalla el tento HelloWorld!
+Autor: Pablo Sebastian Ranieri Castillo
+Ultima fecha de modificacion: 13/6/2017
+*/
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
