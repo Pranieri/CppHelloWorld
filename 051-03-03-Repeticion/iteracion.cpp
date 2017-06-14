c4b3c2a7895a6a3922e554f72b317be66408eb80
Pranieri <sebastian.ranieri@hotmail.com>
2017-06-13 23:50:33 -0300

    Add files via upload

---
+++ 051-03-03-Repeticion/iteracion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/*
+Nombre: Iteracion
+Descripcion: Se repite una fase 30 veces
+Autor: Pablo Sebastian Ranieri Castillo
+Ultima fecha de modificacion: 13/6/2017
+*/
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i<30; i++)
+    cout << "Repeticion" <<endl;
+
+    return 0;
+}
